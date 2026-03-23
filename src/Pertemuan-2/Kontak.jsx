fe815c2015942ad73d680cca0d4e07abbe13d0c0
naurarahmafadilah <naura24si@mahasiswa.pcr.ac.id>
2026-03-23 10:38:42 +0700

    Perubahan Email

--- src/Pertemuan-2/Kontak.jsx
+++ src/Pertemuan-2/Kontak.jsx
@@ -2,7 +2,7 @@ export default function Kontak(){
   return(
     <div>
       <h2>Kontak</h2>
-      <p>Email : naura@email.com</p>
+      <p>Email : naura24si@mahasiswa.pcr.ac.id</p>
       <p>Instagram : @naura</p>
       <p>Github : github.com/naura</p>
     </div>
